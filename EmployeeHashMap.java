9ae7ea821682aa2673ecd2335025d1f379f0b156
Pranjal Tripathi <pranjaltripathi777@gmail.com>
2026-07-31 16:28:17 +0530

    EmployeeHashMap.java

--- EmployeeHashMap.java
+++ EmployeeHashMap.java
@@ -1,8 +1,15 @@
-package com.practice;
-import java.util.*;
-public class EmployeeHashMap{
-public static void main(String[] args){
-HashMap<Integer,String> map=new HashMap<>();
-map.put(101,"John"); map.put(102,"Sara");
-System.out.println(map.get(101));
-}}
+import java.util.HashMap;
+
+public class EmployeeHashMap {
+
+    public static void main(String[] args) {
+
+        HashMap<Integer, String> employeeMap = new HashMap<>();
+
+        employeeMap.put(101, "John");
+        employeeMap.put(102, "Sara");
+
+        System.out.println("Employee ID 101: " + employeeMap.get(101));
+        System.out.println("Employee ID 102: " + employeeMap.get(102));
+    }
+}
