b05a93e12b8eb38c05176a872fde01dd4565c789
Pranjal Tripathi <pranjaltripathi777@gmail.com>
2026-07-31 16:27:25 +0530

    BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,7 +1,17 @@
-package com.practice;
-import java.util.*;
-public class BinarySearch{
-public static void main(String[] args){
-int[] arr={1,3,5,7,9};
-System.out.println(Arrays.binarySearch(arr,7));
-}}
+import java.util.Arrays;
+
+public class BinarySearch {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 3, 5, 7, 9};
+
+        int index = Arrays.binarySearch(arr, 7);
+
+        if (index >= 0) {
+            System.out.println("Element found at index: " + index);
+        } else {
+            System.out.println("Element not found.");
+        }
+    }
+}
