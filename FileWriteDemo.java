06d9778a26c9c0dd6f846d60d2d6907739193db8
Pranjal Tripathi <pranjaltripathi777@gmail.com>
2026-07-31 16:29:34 +0530

    FileWriteDemo.java

--- FileWriteDemo.java
+++ FileWriteDemo.java
@@ -1,7 +1,20 @@
-package com.practice;
-import java.io.*;
-public class FileWriteDemo{
-public static void main(String[] args)throws Exception{
-FileWriter fw=new FileWriter("output.txt");
-fw.write("Hello Java"); fw.close();
-}}
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileWriteDemo {
+
+    public static void main(String[] args) {
+
+        try {
+            FileWriter writer = new FileWriter("output.txt");
+
+            writer.write("Hello Java");
+            writer.close();
+
+            System.out.println("Data written successfully to output.txt");
+
+        } catch (IOException e) {
+            System.out.println("Error writing to file: " + e.getMessage());
+        }
+    }
+}
