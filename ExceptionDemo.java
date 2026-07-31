4b7f6818a046a38ecc9680b3c6c5d12d3e4fb65d
Pranjal Tripathi <pranjaltripathi777@gmail.com>
2026-07-31 16:28:43 +0530

    ExceptionDemo.java

--- ExceptionDemo.java
+++ ExceptionDemo.java
@@ -1,6 +1,14 @@
-package com.practice;
-public class ExceptionDemo{
-public static void main(String[] args){
-try{int x=10/0;}catch(Exception e){
-System.out.println("Handled: "+e.getMessage());
-}}}
+public class ExceptionDemo {
+
+    public static void main(String[] args) {
+
+        try {
+            int result = 10 / 0;
+            System.out.println(result);
+        } catch (ArithmeticException e) {
+            System.out.println("Exception Handled: " + e.getMessage());
+        }
+
+        System.out.println("Program executed successfully.");
+    }
+}
