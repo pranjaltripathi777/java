00553cc6a1c552d8e7c830115cd049087de9b017
Pranjal Tripathi <pranjaltripathi777@gmail.com>
2026-07-31 16:31:14 +0530

    StudentManagement.java

--- StudentManagement.java
+++ StudentManagement.java
@@ -1,14 +1,32 @@
-package com.practice;
-import java.util.*;
-class Student{
-int id;String name;
-Student(int id,String name){this.id=id;this.name=name;}
-public String toString(){return id+" - "+name;}
+import java.util.ArrayList;
+
+class Student {
+
+    private int id;
+    private String name;
+
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student ID: " + id + ", Name: " + name;
+    }
 }
-public class StudentManagement{
-public static void main(String[] args){
-ArrayList<Student> list=new ArrayList<>();
-list.add(new Student(1,"Aman"));
-list.add(new Student(2,"Riya"));
-list.forEach(System.out::println);
-}}
+
+public class StudentManagement {
+
+    public static void main(String[] args) {
+
+        ArrayList<Student> students = new ArrayList<>();
+
+        students.add(new Student(1, "Aman"));
+        students.add(new Student(2, "Riya"));
+
+        System.out.println("Student Details:");
+
+        students.forEach(System.out::println);
+    }
+}
