4c19739c5be33f35bab8baa3a07db9766e04a183
Pranjal Tripathi <pranjaltripathi777@gmail.com>
2026-07-31 16:29:58 +0530

    FrequencyCounter.java

--- FrequencyCounter.java
+++ FrequencyCounter.java
@@ -1,9 +1,18 @@
-package com.practice;
-import java.util.*;
-public class FrequencyCounter{
-public static void main(String[] args){
-String s="banana";
-HashMap<Character,Integer> m=new HashMap<>();
-for(char c:s.toCharArray())m.put(c,m.getOrDefault(c,0)+1);
-System.out.println(m);
-}}
+import java.util.HashMap;
+
+public class FrequencyCounter {
+
+    public static void main(String[] args) {
+
+        String text = "banana";
+
+        HashMap<Character, Integer> frequencyMap = new HashMap<>();
+
+        for (char ch : text.toCharArray()) {
+            frequencyMap.put(ch, frequencyMap.getOrDefault(ch, 0) + 1);
+        }
+
+        System.out.println("Character Frequency:");
+        System.out.println(frequencyMap);
+    }
+}
