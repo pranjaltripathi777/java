ab221bbc18aabecd9b1c66277dd46bd6e1e498e2
Pranjal Tripathi <pranjaltripathi777@gmail.com>
2026-07-31 16:27:53 +0530

    BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,9 +1,25 @@
-package com.practice;
-public class BubbleSort{
-public static void main(String[] args){
-int[] a={5,1,4,2};
-for(int i=0;i<a.length;i++)
-for(int j=0;j<a.length-i-1;j++)
-if(a[j]>a[j+1]){int t=a[j];a[j]=a[j+1];a[j+1]=t;}
-for(int x:a) System.out.print(x+" ");
-}}
+public class BubbleSort {
+
+    public static void main(String[] args) {
+
+        int[] arr = {5, 1, 4, 2};
+
+        for (int i = 0; i < arr.length - 1; i++) {
+
+            for (int j = 0; j < arr.length - i - 1; j++) {
+
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        System.out.print("Sorted Array: ");
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
