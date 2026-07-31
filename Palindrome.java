4264c19fe521b17845b32f2c0697a99fb29b3c66
Pranjal Tripathi <pranjaltripathi777@gmail.com>
2026-07-31 16:30:22 +0530

    Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -1,7 +1,15 @@
-package com.practice;
-public class Palindrome{
-public static void main(String[] args){
-String s="madam";
-String r=new StringBuilder(s).reverse().toString();
-System.out.println(s.equals(r));
-}}
+public class Palindrome {
+
+    public static void main(String[] args) {
+
+        String text = "madam";
+
+        String reversedText = new StringBuilder(text).reverse().toString();
+
+        if (text.equals(reversedText)) {
+            System.out.println(text + " is a palindrome.");
+        } else {
+            System.out.println(text + " is not a palindrome.");
+        }
+    }
+}
