6fa315ec09ba98667d5a7f62f452f3a2734b840d
Pranjal Tripathi <pranjaltripathi777@gmail.com>
2026-07-31 16:30:50 +0530

    StreamAPIExample.java

--- StreamAPIExample.java
+++ StreamAPIExample.java
@@ -1,7 +1,17 @@
-package com.practice;
-import java.util.*;
-public class StreamAPIExample{
-public static void main(String[] args){
-List<Integer> list=Arrays.asList(5,2,8,1,9);
-list.stream().filter(x->x>4).sorted().forEach(System.out::println);
-}}
+import java.util.Arrays;
+import java.util.List;
+
+public class StreamAPIExample {
+
+    public static void main(String[] args) {
+
+        List<Integer> numbers = Arrays.asList(5, 2, 8, 1, 9);
+
+        System.out.println("Numbers greater than 4 (Sorted):");
+
+        numbers.stream()
+               .filter(number -> number > 4)
+               .sorted()
+               .forEach(System.out::println);
+    }
+}
